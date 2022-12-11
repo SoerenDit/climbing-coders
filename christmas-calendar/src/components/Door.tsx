fbe7b7ac0421d6eb278329963ae908442e2cf93f
Søren Ditlefsen <sredit@norlys.dk>
2022-12-11 15:54:47 +0100

    Disable buttons of future date

--- christmas-calendar/src/components/Door.tsx
+++ christmas-calendar/src/components/Door.tsx
@@ -8,17 +8,17 @@ import { IDoor } from "../models/IDoor";
 const Door = (props: { door: IDoor }) => {
   const door = props.door;
   const [open, setOpen] = useState(false);
+
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
+  var today = Number(String(new Date().getDate()).padStart(2, '0'));
+
+
   return (
     <div>
-      <Button onClick={handleOpen} sx={buttonStyle}>
-        <Typography
-          id="button-text"
-          variant="h1"
-          color="darkred"
-        >
+      <Button onClick={handleOpen} sx={buttonStyle} disabled={door.day > today}>
+        <Typography id="button-text" variant="h1" color="darkred">
           {door.day}
         </Typography>
       </Button>
@@ -29,10 +29,10 @@ const Door = (props: { door: IDoor }) => {
         aria-describedby="modal-modal-description"
       >
         <Box id="snow" sx={modalStyle}>
-          <Typography id="modal-modal-title" variant="h4" align="center">
-            {door.title}
+          <Typography id="modal-modal-title" variant="h2" align="center" color="darkred">
+            {door.title} {door.day <= today ? "åben" : "lukket"}
           </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+          <Typography id="modal-modal-description" variant="h4" color="darkred">
             {door.description}
           </Typography>
         </Box>
@@ -53,12 +53,15 @@ const buttonStyle = {
 
 const modalStyle = {
   position: "absolute",
+  borderRadius: "20px",
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
   width: "90vh",
   height: "90vh",
-  bgcolor: "background.paper",
+  background:
+    "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%)",
+  // bgcolor: "background.paper",
   border: "2px solid #000",
   boxShadow: 24,
   p: 4,
