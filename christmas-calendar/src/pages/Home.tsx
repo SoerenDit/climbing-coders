a542d46ce2fddd53dc8d9592eea609b51e697d5b
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2022-12-11 14:40:41 +0100

    Added map for doors

--- christmas-calendar/src/pages/Home.tsx
+++ christmas-calendar/src/pages/Home.tsx
@@ -1,5 +1,12 @@
+import { Door } from "../components/Door";
+import { Storage } from "../services/storage";
+
 export const Home = () => {
+    const doors = Storage.getDoors();
+
     return (
-        <h1>Home</h1>
+        <div>
+            {doors.map(d => <Door door={d}></Door>)}
+        </div>
     );
 }
