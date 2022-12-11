080aa716459f65bc7f63dac4304525a88f5a1e01
Hindubedu <40704028+Hindubedu@users.noreply.github.com>
2022-12-11 15:06:53 +0100

    To all my HOMEies

--- christmas-calendar/src/pages/NotFound.tsx
+++ christmas-calendar/src/pages/NotFound.tsx
@@ -1,5 +1,17 @@
-export const NotFound = () => {
+import React from "react";
+import { HomeButton } from "../components/HomeButton";
+import { BrowserHistory } from "@remix-run/router";
+import { useNavigate } from "react-router";
+import { Button } from "@mui/material";
+
+export function NotFound() {
+    const navigate = useNavigate();
+    const navigateTo = () => navigate("/");
+
+
     return (
-        <div>NotFound</div>
+        <div>
+            <Button variant="contained" onClick={navigateTo} size="large" color="error" >HO-HO-HOME</Button>
+        </div>
     );
 }
