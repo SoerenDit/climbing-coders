00e326bfe011bb9b15c299210839011df520a11b
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2022-12-11 16:15:43 +0100

    Big cleanup

--- christmas-calendar/src/pages/NotFound.tsx
+++ christmas-calendar/src/pages/NotFound.tsx
@@ -1,17 +1,14 @@
-import React from "react";
-import { HomeButton } from "../components/HomeButton";
-import { BrowserHistory } from "@remix-run/router";
-import { useNavigate } from "react-router";
 import { Button } from "@mui/material";
+import React from "react";
+import { useNavigate } from "react-router";
 
 export function NotFound() {
     const navigate = useNavigate();
     const navigateTo = () => navigate("/");
 
-
     return (
         <div>
-            <Button variant="contained" onClick={navigateTo} size="large" color="error" >HO-HO-HOME</Button>
+            <Button variant="contained" onClick={navigateTo} size="large" color="error">HO-HO-HOME</Button>
         </div>
     );
 }
